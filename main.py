f9f1c8cefb2562f1065e209683dd638a68d1b0e1
DipayanCoder25 <dipayanprogramer3@gmail.com>
2025-04-24 17:14:29 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# a square program without using loop
+import turtle
+
+polygon = turtle.Turtle()
+turtle.Screen().bgcolor("Blue")
+
+polygon.forward(120)
+polygon.right(90)
+polygon.fd(120)
+polygon.right(90)
+polygon.forward(120)
+polygon.right(90)
+polygon.fd(120)
