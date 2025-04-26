a8303b926e9085cb84ef6773196d892e19195995
DipayanCoder25 <dipayanprogramer3@gmail.com>
2025-04-26 12:45:38 +0600

    Add files via upload

---
+++ projects.py
@@ -0,0 +1,158 @@
+# this code transfers a decimal number to binary number
+'''
+decimal_number = int(input("Enter a decimal number: "))
+
+binary_number = ""
+number = decimal_number 
+
+
+while number > 0:
+    remainder = number % 2
+    binary_number = str(remainder) + binary_number
+    number = number // 2
+
+if binary_number == "":
+    binary_number = "0"
+
+print(f"Binary of {decimal_number} is {binary_number}")
+
+
+
+rows = int(input("Please Enter the Total Number of Rows  : "))
+
+for i in range(1, rows + 1):
+    for j in range(1, rows + 1):
+        if(j <= rows - i):
+            print(' ', end = '  ')
+        else:
+            print('*', end = '  ')
+    print()
+import turtle
+
+import turtle
+
+polygon = turtle.Turtle()
+turtle.Screen().bgcolor("Blue")
+polygon.speed(30)
+polygon.forward(120)
+polygon.right(90)
+polygon.fd(120)
+polygon.right(90)
+polygon.forward(120)
+polygon.right(90)
+polygon.fd(120)
+polygon.penup()
+polygon.pendown()
+polygon.forward(120)
+polygon.right(90)
+polygon.fd(120)
+polygon.right(90)
+polygon.forward(120)
+polygon.left(90)
+polygon.forward(120)
+polygon.right(90)
+polygon.fd(120)
+polygon.right(90)
+polygon.forward(120)
+polygon.fd(240)
+polygon.right(90)
+polygon.fd(120)
+polygon.right(90)
+polygon.forward(120)
+polygon.right(90)
+polygon.right(90)
+polygon.fd(120)
+polygon.right(90)
+polygon.fd(120)
+polygon.right(90)
+polygon.fd(120)
+polygon.left(90)
+polygon.fd(120)
+polygon.left(90)
+polygon.fd(120)
+polygon.left(90)
+polygon.fd(120)
+polygon.penup()
+
+polygon.pendown()
+polygon.right(90)
+polygon.back(120)
+polygon.left(90)
+polygon.left(90)
+polygon.fd(240)
+polygon.right(90)
+polygon.fd(120)
+polygon.right(90)
+polygon.fd(120)
+polygon.right(90)
+
+polygon.fd(240)
+polygon.left(90)
+polygon.fd(120)
+
+polygon.left(90)
+polygon.fd(120)
+polygon.left(90)
+polygon.fd(240)
+polygon.left(90)
+polygon.fd(120)
+polygon.left(90)
+polygon.fd(120)
+polygon.left(90)
+polygon.fd(120)
+
+
+
+polygon.done
+polygon.penup()
+'''
+#draws like a flower
+'''
+import turtle
+tur=turtle.Turtle()
+tur.color("Red")
+
+tur.speed(300)
+for i in range(60):
+  tur.goto(12,12)
+  tur.left(-79)
+  tur.fd(100)
+ 
+import turtle
+tur=turtle.Turtle()
+tur.color("Aqua")
+
+tur.speed(300)
+for i in range(60):
+  tur.goto(12,12)
+  tur.left(79)
+  tur.fd(100)
+  tur.pendown()
+
+tur.goto(10,-90)
+tur.right(150)
+tur.color("Red")
+tur.pensize(1)
+tur.fd(200)
+tur.back(200)
+tur.left(1.3)
+tur.fd(200)  
+ 
+#Draws a beatiful ...
+import turtle
+tur=turtle.Turtle()
+tur.color("Aqua")
+tur.speed(1000)
+tur.fd(120)
+for i in range(120):
+  tur.left(156)
+  tur.fd(120+i+1)
+  '''
+import turtle
+tur=turtle.Turtle()
+tur.color("Aqua")
+tur.speed(1000)
+tur.fd(120)
+for i in range(120):
+  tur.left(89)
+  tur.fd(120+i+1)  
